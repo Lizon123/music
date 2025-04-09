d0bfb4cbbf551c7136302d5ddaec6f005f3dd7b0
Liza <lizaolek145@gmail.com>
2025-04-09 23:18:04 +0300

    create part with music album

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const menuToggle = document.getElementById("mobile-menu");
-        const burgerMenu = document.getElementById("burger-menu");
-        let menuOpen = false;
+    const burgerMenu = document.getElementById("burger-menu");
+    let menuOpen = false;
         
-        menuToggle.addEventListener("click", () => {
-            menuOpen = !menuOpen;
-            burgerMenu.style.left = menuOpen ? "0" : "-250px";
-        });
+    menuToggle.addEventListener("click", () => {
+        menuOpen = !menuOpen;
+        burgerMenu.style.left = menuOpen ? "0" : "-250px";
+    });
+
+function filterPlaylists(category) {
+    const allTiles = document.querySelectorAll('.playlist-tile');
+    allTiles.forEach(tile => {
+    if (category === 'all' || tile.dataset.category === category) {
+        tile.classList.remove('hidden');
+    } else {
+        tile.classList.add('hidden');
+    }
+});
